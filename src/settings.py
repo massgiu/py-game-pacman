2de64a1a6b6d56274621468234d02e40823c2487
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-05-23 16:14:24 +0200

    Initial commit

---
+++ src/settings.py
@@ -0,0 +1,10 @@
+#Screen settings
+WIDTH, HEIGHT = 448, 595
+FPS = 60
+#color settings
+BLACK = (0, 0, 0)
+OCHER = (170, 132, 58)
+#font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial_black'
+#player settings
