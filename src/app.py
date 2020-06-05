6e3cee61aba21d55d1cc0f7c261e607286812f32
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-06-05 22:13:47 +0200

    Simplified code in app.py creating some class methods in utils.py

--- src/app.py
+++ src/app.py
@@ -16,9 +16,7 @@ class App:
         self.running = True
         self.state = 'start'
         self.enemies = []
-        self.walls, self.coins, self.player_start_pos, self.enemies_start_pos = self.load()  # load background and init pos
-        self.player = Player(self, vec(self.player_start_pos))
-        self.make_enemies(self.enemies_start_pos)
+        self.init()
 
     def run(self):
         while self.running:
@@ -40,27 +38,10 @@ class App:
         pygame.quit()
         sys.exit()
 
-    def draw_text(self, words, screen, pos, size, color, font_name, centered=False):
-        font = pygame.font.SysFont(font_name, size)
-        text = font.render(words, False, color)
-        text_size = text.get_size()
-        pos = list(pos)  # tuple are immutable, so list conversion
-        if centered:
-            pos[0] = pos[0] - text_size[0] // 2
-            pos[1] = pos[1] - text_size[1] // 2
-        screen.blit(text, tuple(pos))
-
-    def draw_grid(self):
-        # vertical lines
-        for x in range(WIDTH // CELL_W):
-            pygame.draw.line(self.background, GREY, (CELL_W * x, 0), (CELL_W * x, HEIGHT))
-        # horizontal lines
-        for y in range(WIDTH // CELL_H):
-            pygame.draw.line(self.background, GREY, (0, CELL_H * y), (WIDTH, CELL_H * y))
-        # draw lane
-        # for coin in self.coins:
-        #     pygame.draw.rect(self.background, YELLOW,
-        #                      (coin[0]*self.cell_width, coin[1]*self.cell_height, self.cell_width, self.cell_height))
+    def init(self):
+        self.walls, self.coins, self.player_start_pos, self.enemies_start_pos = self.load()  # load background and init pos
+        self.player = Player(self, vec(self.player_start_pos))
+        self.make_enemies(self.enemies_start_pos)
 
     def load(self):
         self.background = pygame.image.load(BACKGROUND_IMG_URL)
@@ -68,12 +49,11 @@ class App:
         walls, coins, player_start_pos, enemies_start_pos = Utils.read_layout(self.background)
         return walls, coins, player_start_pos, enemies_start_pos
 
-    # Start functions
-
     def make_enemies(self, enemies_pos):
         for index, enemy_pos in enumerate(enemies_pos):
             self.enemies.append(Enemy(self, vec(enemy_pos), index))
 
+    # Start functions
     def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,12 +67,11 @@ class App:
 
     def start_draw(self):
         self.screen.fill(BLACK)
-        self.draw_text('PUSH SPACE START', self.screen, (WIDTH // 2, HEIGHT // 2 - 50), START_TEXT_SIZE, OCHER,
+        Utils.draw_text('PUSH SPACE START', self.screen, (WIDTH // 2, HEIGHT // 2 - 50), START_TEXT_SIZE, OCHER,
                        START_FONT, True)
-        self.draw_text('1 PLAYER ONLY', self.screen, (WIDTH // 2, HEIGHT // 2 + 50), START_TEXT_SIZE, LIGHT_BLUE,
+        Utils.draw_text('1 PLAYER ONLY', self.screen, (WIDTH // 2, HEIGHT // 2 + 50), START_TEXT_SIZE, LIGHT_BLUE,
                        START_FONT, True)
-        self.draw_text('HIGH SCORE', self.screen, (4, 0), START_TEXT_SIZE, WHITE, START_FONT)
-
+        Utils.draw_text('HIGH SCORE', self.screen, (4, 0), START_TEXT_SIZE, WHITE, START_FONT)
         pygame.display.update()
 
     # Playing functions
@@ -120,24 +99,17 @@ class App:
                 self.remove_life(self.player_start_pos, self.enemies_start_pos)
 
     def playing_draw(self):
-        # clock = pygame.time.Clock()
-        # clock.tick(10)
         self.screen.fill(BLACK)
         self.screen.blit(self.background, (TOP_BOTTOM_BUFFER // 2, TOP_BOTTOM_BUFFER // 2))
-        self.draw_coins()
-        self.draw_grid()
-        self.draw_text(f'CURRENT SCORE: {self.player.current_score}', self.screen, (60, 0), 18, WHITE, START_FONT,
+        Utils.draw_coins(self.screen, self.coins)
+        Utils.draw_grid(self.background)
+        Utils.draw_text(f'CURRENT SCORE: {self.player.current_score}', self.screen, (60, 0), 18, WHITE, START_FONT,
                        False)
-        self.draw_text(f'HIGH SCORE: {0}', self.screen, (WIDTH // 2 + 60, 0), 18, WHITE, START_FONT, False)
+        Utils.draw_text(f'HIGH SCORE: {0}', self.screen, (WIDTH // 2 + 60, 0), 18, WHITE, START_FONT, False)
         self.player.draw()
         for index, enemy in enumerate(self.enemies):
             enemy.draw(index)
         pygame.display.update()
-
-    def draw_coins(self):
-        for coin in self.coins:
-            pygame.draw.circle(self.screen, COINS_COLOR, (int(coin.x * CELL_W + CELL_W // 2) + TOP_BOTTOM_BUFFER // 2,
-                          int(coin.y * CELL_H + CELL_H // 2) + TOP_BOTTOM_BUFFER // 2),5)
 
     def remove_life(self, player_start_pos, enemies_start_pos):
         self.player.lives -= 1
@@ -154,6 +126,8 @@ class App:
                 enemy.pix_pos = enemy.from_grid_to_pix_pos(enemy.grid_pos)
                 enemy.direction = NEUTRAL
 
+    # Game Over functions
+
     def game_over_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -169,12 +143,12 @@ class App:
 
     def game_over_draw(self):
         self.screen.fill(BLACK)
-        self.draw_text('GAME OVER', self.screen, (WIDTH // 2, HEIGHT // 2 - 50), START_TEXT_SIZE, RED,
+        Utils.draw_text('GAME OVER', self.screen, (WIDTH // 2, HEIGHT // 2 - 50), START_TEXT_SIZE, RED,
                        START_FONT, True)
-        self.draw_text('Press the SPACE BAR to continue', self.screen, (WIDTH // 2, HEIGHT // 2 - 20), START_TEXT_SIZE,
+        Utils.draw_text('Press the SPACE BAR to continue', self.screen, (WIDTH // 2, HEIGHT // 2 - 20), START_TEXT_SIZE,
                        WHITE,
                        START_FONT, True)
-        self.draw_text('Press ESCAPE to quit', self.screen, (WIDTH // 2, HEIGHT // 2), START_TEXT_SIZE,
+        Utils.draw_text('Press ESCAPE to quit', self.screen, (WIDTH // 2, HEIGHT // 2), START_TEXT_SIZE,
                        WHITE, START_FONT, True)
         pygame.display.update()
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import Vector2 as vec
 from src.settings import *
+from src.enemy import *
+
 class Utils:
 
     def __init__(self):
@@ -26,3 +28,34 @@ class Utils:
                         pygame.draw.rect(background, BLACK, (width * CELL_W, height * CELL_H,
                                                          CELL_W, CELL_H))
         return walls, coins, player_start_pos, enemies_start_pos
+
+    @classmethod
+    def draw_text(self, words, screen, pos, size, color, font_name, centered=False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, color)
+        text_size = text.get_size()
+        pos = list(pos)  # tuple are immutable, so list conversion
+        if centered:
+            pos[0] = pos[0] - text_size[0] // 2
+            pos[1] = pos[1] - text_size[1] // 2
+        screen.blit(text, tuple(pos))
+
+    @classmethod
+    def draw_grid(self, background):
+        # vertical lines
+        for x in range(WIDTH // CELL_W):
+            pygame.draw.line(background, GREY, (CELL_W * x, 0), (CELL_W * x, HEIGHT))
+        # horizontal lines
+        for y in range(WIDTH // CELL_H):
+            pygame.draw.line(background, GREY, (0, CELL_H * y), (WIDTH, CELL_H * y))
+        # draw lane
+        # for coin in self.coins:
+        #     pygame.draw.rect(self.background, YELLOW,
+        #                      (coin[0]*self.cell_width, coin[1]*self.cell_height, self.cell_width, self.cell_height))
+
+    @classmethod
+    def draw_coins(self, screen, coins):
+        for coin in coins:
+            pygame.draw.circle(screen, COINS_COLOR, (int(coin.x * CELL_W + CELL_W // 2) + TOP_BOTTOM_BUFFER // 2,
+                          int(coin.y * CELL_H + CELL_H // 2) + TOP_BOTTOM_BUFFER // 2),5)
+
